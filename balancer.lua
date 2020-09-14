3a358c682d7c32d9f089e6ffa67913cba4321698
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-09-14 17:37:02 +0700

    Add: access_by_lua_block

--- balancer.lua
+++ balancer.lua
@@ -2,9 +2,8 @@ local balancer = require "ngx.balancer"
 -- well, usually we calculate the peer's host and port
 -- according to some balancing policies instead of using
 -- hard-coded values like below
-local req_headers, err = ngx.req.get_headers()
 
-local upstream_id = req_headers['Ip-Id']
+ngx.log(ngx.WARN, 'COMING HERE: ', ngx.ctx.ip_id)
 
 local host = "127.0.0.2"
 local port = 8080
@@ -13,4 +12,3 @@ if not ok then
     ngx.log(ngx.ERR, "failed to set the current peer: ", err)
     return ngx.exit(500)
 end
-ngx.log(ngx.WARN, 'COMING HERE: ', upstream_id)
