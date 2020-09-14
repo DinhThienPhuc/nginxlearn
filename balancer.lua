5f9039eecd0bfb7b6c8acd9056caff0c08f6755b
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-09-14 15:15:58 +0700

    Mod: with balancer logic

--- balancer.lua
+++ balancer.lua
@@ -1,12 +1,12 @@
--- local balancer = require "ngx.balancer"
+local balancer = require "ngx.balancer"
 -- well, usually we calculate the peer's host and port
 -- according to some balancing policies instead of using
 -- hard-coded values like below
--- local host = "127.0.0.2"
--- local port = 8080
--- local ok, err = balancer.set_current_peer(host, port)
--- if not ok then
---     ngx.log(ngx.ERR, "failed to set the current peer: ", err)
---     return ngx.exit(500)
--- end
+local host = "127.0.0.2"
+local port = 8080
+local ok, err = balancer.set_current_peer(host, port)
+if not ok then
+    ngx.log(ngx.ERR, "failed to set the current peer: ", err)
+    return ngx.exit(500)
+end
 ngx.log(ngx.WARN, 'COMING HERE')
